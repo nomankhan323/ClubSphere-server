dee7ca2016f69f03793b3072ccc0957569d3391e
Noman <nomankhan192069@gmail.com>
2025-12-11 12:45:33 +0600

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,17 +1,1 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyCzL1DzqoY2sYcAhvg6ND7CbC_gHwOg5N4",
-    authDomain: "clubsphere-d4b8e.firebaseapp.com",
-    projectId: "clubsphere-d4b8e",
-    storageBucket: "clubsphere-d4b8e.firebasestorage.app",
-    messagingSenderId: "302878652378",
-    appId: "1:302878652378:web:3901e85aa6cfa1f5fa32e3"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
